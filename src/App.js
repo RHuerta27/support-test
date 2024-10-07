60e33b1c26af7fd4f53819e2cbf40f6e0d932bc1
RHuerta27 <ivanfix05@gmail.com>
2024-10-07 12:34:16 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Aquí se importa tu archivo CSS
+import '@fontsource/poppins'; // Fuente Poppins desde Google Fonts
 
 function App() {
+  const [textoQueLlego, setTextoQueLlego] = useState('');
+  const [contador, setContador] = useState(0);
+
+  const handleButtonClick = async () => {
+    setContador(contador + 1);
+    try {
+      const response = await fetch('https://memeado.vercel.app/api/hello');
+      const data = await response.json();
+      setTextoQueLlego(data.name || 'Sin respuesta');
+    } catch (error) {
+      setTextoQueLlego('Error en la solicitud');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <h1>LogoTest</h1>
+        <button className="signIn">Sign In</button>
       </header>
+      <main className="main-content">
+        <h2>Presiona un botón y haz la prueba</h2>
+        <button className="solicitar-button" onClick={handleButtonClick}>
+          Solicitar
+        </button>
+        <p>
+          Llegó: <strong>{textoQueLlego}</strong>
+        </p>
+        <p>
+          Se ha presionado: <strong>{contador}</strong> veces
+        </p>
+      </main>
     </div>
   );
 }
